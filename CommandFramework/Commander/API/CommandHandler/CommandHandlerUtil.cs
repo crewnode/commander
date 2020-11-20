213979791b552638023b4c48f0b8521814e1d936
dj346 <djgaming346@gmail.com>
2020-11-19 20:40:25 -0800

    Removed unnecessary things

--- CommandFramework/Commander/API/CommandHandler/CommandHandlerUtil.cs
+++ CommandFramework/Commander/API/CommandHandler/CommandHandlerUtil.cs
@@ -3,8 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CommandFramework.API.CommandHandler
+namespace CommandFramework.API.CommandHandler
 {
     class CommandHandlerUtil
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using CommandFramework.API.Attributes;
+    using CommandFramework.Managers.Models;
+
+    public static class CommandHandlerUtil
     {
+        public static IEnumerable<Command> LoadModulesFromFile(string filePath, params object[] commandArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Command LoadModule(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Command> LoadModules(Command[] comands)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
